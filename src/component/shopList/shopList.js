77054c96c4c3d1ee43a90e59da85cd01d347bbdd
pkkwilliam <pkkwilliam919@gmail.com>
2021-01-22 10:44:43 +0800

    add images for shops

--- src/component/shopList/shopList.js
+++ src/component/shopList/shopList.js
@@ -1,9 +1,13 @@
 import React from "react";
 import ClientApplicationComponent from "../clientApplicationComponent";
-import { GET_GITHUB_JSON_CONTENT } from "online-shopping-cargo-parent/dist/service";
+import {
+  GET_GITHUB_JSON_CONTENT,
+  GITHUB_CONTENT_URL,
+} from "online-shopping-cargo-parent/dist/service";
 import LineBreak from "online-shopping-cargo-parent/dist/lineBreak";
 import P from "online-shopping-cargo-parent/dist/text/paragraph";
 import ShadowCard from "../common/shadowCard";
+import Accordion from "react-bootstrap/esm/Accordion";
 
 export default class ShopList extends ClientApplicationComponent {
   state = {
@@ -23,32 +27,50 @@ export default class ShopList extends ClientApplicationComponent {
 
   render() {
     return (
-      <div>
+      <Accordion>
         <ShopsDisplay shops={this.state.shops} />
-      </div>
+      </Accordion>
     );
   }
 }
 
 function ShopsDisplay({ shops }) {
-  return shops.map((shop) => <ShopCard shop={shop} />);
+  return shops.map((shop, index) => <ShopCard index={index + 1} shop={shop} />);
 }
 
-function ShopCard({ shop }) {
+function ShopCard({ index, shop }) {
   const {
     openingHour,
     shopAddress,
-    shopPhoneNumber,
+    shopFrontImage,
+    shopMapImage,
     shopName,
     shopNumber,
+    shopPhoneNumber,
   } = shop;
   return (
     <ShadowCard style={{ flexDirection: "column" }}>
-      <P
-        style={{ fontSize: "1rem", fontWeight: 600 }}
-      >{`店號:${shopNumber} ${shopName}`}</P>
-      <LineBreak style={{ marginBottom: 5, marginTop: 5 }} />
-      <P>{shopAddress}</P>
+      <Accordion.Toggle as={P} eventKey={index}>
+        <>
+          <P
+            style={{ fontSize: "1rem", fontWeight: 600 }}
+          >{`店號:${shopNumber} ${shopName}`}</P>
+          <LineBreak style={{ marginBottom: 5, marginTop: 5 }} />
+          <P>{shopAddress}</P>
+        </>
+      </Accordion.Toggle>
+      <Accordion.Collapse eventKey={index}>
+        <div>
+          <P>{`營業時間: ${openingHour}`}</P>
+          <P>{`電話: ${shopPhoneNumber}`}</P>
+          <ShopImage imageUrl={`${GITHUB_CONTENT_URL}${shopMapImage}`} />
+          <ShopImage imageUrl={`${GITHUB_CONTENT_URL}${shopFrontImage}`} />
+        </div>
+      </Accordion.Collapse>
     </ShadowCard>
   );
+}
+
+function ShopImage({ imageUrl }) {
+  return <img src={imageUrl} style={{ width: "100%" }} />;
 }
