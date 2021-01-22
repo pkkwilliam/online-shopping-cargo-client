befce53d243d56ab7257a492ce5131e958db6be2
pkkwilliam <pkkwilliam919@gmail.com>
2021-01-22 11:14:47 +0800

    add images for shops

--- src/component/footer/footer.view.js
+++ src/component/footer/footer.view.js
@@ -5,7 +5,6 @@ import P from "online-shopping-cargo-parent/dist/text/paragraph";
 export default function FooterView(props) {
   return (
     <div style={styles.rootContainer}>
-      <P style={{ fontSize: 11 }}>澳門漁翁街166號永好工業大廈7樓F-G座</P>
       <P style={{ fontSize: 11 }}>微信: PICKTB 電話: 28719871 Fax: 28719872</P>
     </div>
   );
