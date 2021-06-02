9b74591a726439fabcf22016aa7ad587c9cca944
pkkwilliam <pkkwilliam919@gmail.com>
2021-06-02 17:12:11 +0800

    add uat

--- src/component/clientApplicationContext.js
+++ src/component/clientApplicationContext.js
@@ -4,4 +4,9 @@ export default class ClientApplicationContext extends applicationContext {
   get mock() {
     return true;
   }
+
+  get serviceUrl() {
+    // REACT_APP_SERVICE_URL=http://bitcode-service:9091 yarn start
+    return process.env.REACT_APP_SERVICE_URL;
+  }
 }
